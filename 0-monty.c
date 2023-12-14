67d0ea9eaee6975af35af1cbeaaa93ccd08827df
Void-Individual <otayomitv@gmail.com>
2023-12-15 00:23:31 +0100

    was the issue wrong error message?

--- 0-monty.c
+++ 0-monty.c
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 			exit(EXIT_SUCCESS);
 		}
 		else
-			printf("Error: Can't open file_d <%s>\n", argv[1]);
+			printf("Error: Can't open file_d %s\n", argv[1]);
 	}
 	else
 		printf("Usage: monty file.m\n");
